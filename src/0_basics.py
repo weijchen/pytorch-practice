be98cafe6cecfb39ab79fc38250a32e5bb062649
weijchen <weijchen0919@gmail.com>
2019-02-15 12:10:14 +0800

    update GPU src

--- src/0_basics.py
+++ src/0_basics.py
@@ -373,7 +373,6 @@ total = 0
 with torch.no_grad():
     for data in testloader:
         images, labels = data
-        images, labels = images.to(device), labels.to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
